019c20bf2a743503a11b1dbb55f698daa4c6bdac
SIMRAN10082001 <simran00220802819@bpitindia.com>
2023-06-03 22:46:20 +0530

    second commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         print("hello world")
+        print("simran arora")
     }
 }
